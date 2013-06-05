3b859c2277f1e574bb30b3f6af5dbbc2ca45d021
Fabien Benureau <fabien.benureau+git@gmail.com>
2013-06-05 20:14:41 +0200

    Tracking proof-of-concept

--- ergotrio.py
+++ ergotrio.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 import pydyn.dynamixel as dyn
 import natnet
@@ -16,7 +17,51 @@ def init_pose():
 
     ctrl.motors[0].position = 170
     ctrl.motors[3].position = 170
+    ctrl.motors[5].position = 100
 
     time.sleep(2)
 
-init_pose()
+ref_point = (-0.1036, 0.2461, 0.1383)
+
+def dist(a, b):
+    assert len(a) == len(b)
+    return sum(abs(a_i - b_i)**2 for a_i, b_i in zip(a, b))
+
+def tip_pos_idx():
+    frame = nnclient.receive_frame()
+    data = frame.unpack_data()
+    sets = data['markersets']
+
+    for s in sets:
+        if s[0] == 'Rigid Body 1':
+            d_ref   = [dist(ref_point, p) for p in s[1]]
+            min_ref = min(d_ref)
+            return d_ref.index(min_ref)
+
+    assert False
+
+def tip_pos(m_idx):
+    frame = nnclient.receive_frame()
+    data = frame.unpack_data()
+    sets = data['markersets']
+
+    for k, s in enumerate(sets):
+        if s[0] == 'Rigid Body 1':
+            return s[1][m_idx]
+
+
+def track():
+    pass
+
+if __name__ == "__main__":
+    #init_pose()
+
+    nnclient = natnet.NatNetClient()
+
+    m_idx = tip_pos_idx()
+    while True:
+        pos = tip_pos(m_idx)
+        if pos is not None:
+            print('{}\r'.format(', '.join('{:7.4f}'.format(e) for e in pos))),
+            sys.stdout.flush()
+        time.sleep(0.01)
