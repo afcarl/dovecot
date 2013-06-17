27e1282bf9cbb6bb23419b7ce37d025a8b1b1c09
Fabien Benureau <fabien.benureau+git@gmail.com>
2013-06-17 16:26:18 +0200

    update vrep_object

--- samples/vrep_object.py
+++ samples/vrep_object.py
@@ -1,18 +1,14 @@
 import testenv
 from toolbox import gfx
 
+import treedict
 from surrogates import vrepsim
 
 def vrep_object():
     """Test the orientation of each joint of the simulation"""
 
-    vs = vrepsim.VRepSim(objname = 'marker')
-
-    # order  = [0.0, 30.0,-80.0, 0.0, 50.0, 0.0]
-    # order += [0.0, 10.0,-70.0, 0.0, 60.0, 0.0]
-    # order += [0.0]
-    # effect = vs.execute_order(order)
-    # print(gfx.repr_vector(effect, fmt='+7.4f'))
+    cfg = treedict.TreeDict()
+    vs = vrepsim.VRepSim(cfg)
 
     order  = [0.0, 30.0,-80.0, 0.0, 50.0, 0.0]
     order += [0.0, 10.0,-74.0, 0.0, 64.0, 0.0]
