ae7a39d414768b73da5d15497ab117be0c46bcdd
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-20 12:12:40 +0100

    same id for all stems

--- examples/tools/change_id.py
+++ examples/tools/change_id.py
@@ -11,12 +11,14 @@ uid = None if len(sys.argv) == 1 else int(sys.argv[1])
 stem = stemcfg.stems[uid]
 stem.cycle_usb()
 
-ms = MotorSet(serial_id=stem.serial_id, motor_range=(0, 253), verbose=True)
+ms = MotorSet(serial_id=stem.serial_id, motor_range=(0, 253), timeout=10, verbose=True)
 
 def change(id_change):
     for m in ms.motors:
         if m.id == id_change[0]:
+            print("changing id of motor {} to {}".format(*id_change))
             m.id = id_change[1]
-    time.sleep(1.0)
+            time.sleep(1.0)
 
-#change((96, 16))
+#change((51, 10))
+#change((52, 11))
