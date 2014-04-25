69740519a9f432bd69a487d76b6cf348181de9ac
Paul Fudal <paul.fudal@inria.fr>
2014-04-25 14:35:41 +0200

    trio_calibrate tool now can receive more then one stem id

--- examples/tools/trio_calibrate.py
+++ examples/tools/trio_calibrate.py
@@ -7,15 +7,21 @@ from dovecot.stemsim import stemcfg
 from dovecot.calibration import triocal
 import powerswitch
 
-stem = stemcfg.stems[int(sys.argv[1])]
-stem.cycle_usb()
+robots = []
 
-ps = powerswitch.Eps4m(mac_address=stemcfg.stems[stem.uid].powerswitch_mac, load_config=True)
-ps_port = stemcfg.stems[stem.uid].powerswitch
-if ps.is_off(ps_port):
-    ps.set_on(ps_port)
-    time.sleep(1)
-while ps.is_restarting(ps_port):
-    time.sleep(1)
+for arg in sys.argv[1:]:
+    robots.append(int(arg))
 
-triocal.calibrate(stem, cached_opti=False, cached_vrep=False)
+for id in robots:
+    stem = stemcfg.stems[id]
+    stem.cycle_usb()
+
+    ps = powerswitch.Eps4m(mac_address=stemcfg.stems[stem.uid].powerswitch_mac, load_config=True)
+    ps_port = stemcfg.stems[stem.uid].powerswitch
+    if ps.is_off(ps_port):
+        ps.set_on(ps_port)
+        time.sleep(1)
+    while ps.is_restarting(ps_port):
+        time.sleep(1)
+
+    triocal.calibrate(stem, cached_opti=False, cached_vrep=False)
