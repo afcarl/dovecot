be8499a9c08b13ecffb85a72bde3781450bde5b3
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-06-16 12:11:30 +0200

    making import HardEnv optional

--- dovecot/__init__.py
+++ dovecot/__init__.py
@@ -2,9 +2,11 @@ from __future__ import absolute_import, division, print_function
 
 from .cfgdesc import desc
 from .vrepsim.sim_env import SimulationEnvironment
-from .stemsim.hard_env import HardwareEnvironment
-from .stemsim.hard_env import stem_uid
+try:
+    from .stemsim.hard_env import HardwareEnvironment
+    from .stemsim.hard_env import stem_uid
+except ImportError:
+    pass
 
 from . import vrepsim
 from . import prims
-
