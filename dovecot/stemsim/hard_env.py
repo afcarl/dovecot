ab02db6a6cdfd5bb0128445c153ce9a6a7ac8237
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-06-26 11:32:46 +0200

    fix typo in collision code

--- dovecot/stemsim/hard_env.py
+++ dovecot/stemsim/hard_env.py
@@ -65,15 +65,19 @@ class HardwareEnvironment(sim_env.SimulationEnvironment):
 
         try:
 
-            motor_poses = self._trajs2poses(motor_traj)
-            ts, motor_poses = self._check_self_collision(motor_traj[0].ts, motor_poses)
-            if not self._check_object_collision(motor_poses):
-                return meta
 
             log =  {}
             meta['log'] = log
             log['motor_command'] = motor_command
             #log['scene'] = 'ar_{}'.format(self.cfg.sprims.scene)
+
+            # check for collisions
+            motor_traj, max_steps = motor_command
+            motor_poses = self._trajs2poses(motor_traj)
+            ts, motor_poses = self._check_self_collision(motor_traj[0].ts, motor_poses)
+            if not self._check_object_collision(motor_poses):
+                return meta
+
 
             if self.verbose:
                 print("{}executing movement on stem...{}".format(gfx.purple, gfx.end), end='\r')
