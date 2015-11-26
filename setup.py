e0e280526c692ce069b1e95f9bf6d995193ab572
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-11-26 09:37:22 +0100

    switch to setuptools and add readme

--- setup.py
+++ setup.py
@@ -1,41 +1,40 @@
-import os
-from distutils.core import setup
+from setuptools import setup
 
 import versioneer
 
+VERSION = '3.1'
+
 setup(
     name         = 'dovecot',
-    version      = '3.0',
+    version      = VERSION,
     cmdclass     = versioneer.get_cmdclass(),
     author       = 'Fabien Benureau, Paul Fudal',
-    author_email = 'fabien.benureau@inria.fr',
-    description  = ('A python library to drive the hardware and simulation experiment of the author PhD thesis'),
-    license      = 'Open Science',
+    author_email = 'fabien.benureau@gmail.com',
+    description  = 'A python library to drive the hardware and simulation experiment of the author PhD thesis',
+    license      = 'Open Science License',
     keywords     = 'science experiment hardware simulation robots',
-    url          = 'flowers.inria.fr',
-    packages = ['dovecot',
-                'dovecot.vizu',
-                'dovecot.prims',
-                'dovecot.ttts',
-                'dovecot.ttts.files',
-                'dovecot.kinsim',
-                'dovecot.vrepsim',
-                'dovecot.stemsim',
-                'dovecot.stemsim',
-                'dovecot.stemsim.stemcfg',
-                'dovecot.collider',
-                'dovecot.calibration',
-                'dovecot.calibration.triocal',
-                'dovecot.calibration.tttcal'
-               ],
-    classifiers = [],
-    package_data = {'dovecot.ttts.files'    : ['vanilla.ttt',
-                                               'vanilla_rightwall.ttt',
-                                               'calibrate.ttt'
-                                               ],
+    url          = 'https://github.com/humm/dovecot',
+    packages     = ['dovecot',
+                    'dovecot.vizu',
+                    'dovecot.prims',
+                    'dovecot.ttts',
+                    'dovecot.ttts.files',
+                    'dovecot.kinsim',
+                    'dovecot.vrepsim',
+                    'dovecot.stemsim',
+                    'dovecot.stemsim',
+                    'dovecot.stemsim.stemcfg',
+                    'dovecot.collider',
+                    'dovecot.calibration',
+                    'dovecot.calibration.triocal',
+                    'dovecot.calibration.tttcal'
+                   ],
+    classifiers  = [],
+    package_data = {'dovecot.ttts.files'    : ['vanilla.ttt'],
                     'dovecot.collider'      : ['stem.smodel'],
                     'dovecot.calibration.triocal' : ['calib0.data',
                                                      'calib1.data',
                                                      'calib2.data',
                                                      'calib3.data']}, # FIXME should be dynamic
+    install_requires = ['scicfg', 'environments', 'numpy', 'sympy'],
 )
